6f132f3193d472ff844cdbf9197c312d4f15c88d
rdjastony <amahajan282@gmail.com>
2025-04-08 16:10:06 +0530

    Initial commit with Jenkinsfile

---
+++ azure/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+ subscription_id = "860103a2-d571-41a9-a19f-532afaceb0bf"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "crud-kube"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ azure/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "ecommerce-rg"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "acr_name" {
+  default = "ecommerceacr123"
+}
+
+variable "aks_name" {
+  default = "ecommerceaks"
+}
